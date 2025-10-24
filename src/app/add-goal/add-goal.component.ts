b1a03b40f1a7bafee2ab278d84584f50c35b8632
Temiloluwa <97484828+Temii-mp3@users.noreply.github.com>
2025-10-24 12:24:44 -0500

    init

---
+++ src/app/add-goal/add-goal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-goal.component',
+  imports: [],
+  templateUrl: './add-goal.component.html',
+  styleUrl: './add-goal.component.css',
+})
+export class AddGoalComponent {}
