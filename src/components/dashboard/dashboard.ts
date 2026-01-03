b0445683ef93710d93719ff6c8a9fd36daaca5a9
Temiloluwa <97484828+Temii-mp3@users.noreply.github.com>
2026-01-03 10:29:03 -0600

    Finished basic UI

--- src/components/dashboard/dashboard.ts
+++ src/components/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialog, MatDialogContent } from '@angular/material/dialog';
@@ -16,6 +16,7 @@ import { NgClass } from '@angular/common';
 import { Goal } from '../../models/goal.model';
 import { EditMenuComponent } from './edit-menu/edit-menu.component';
 import { DeleteMenuComponent } from './delete-menu/delete-menu.component';
+import { MatPaginatorModule, MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-dashboard',
@@ -34,18 +35,44 @@ import { DeleteMenuComponent } from './delete-menu/delete-menu.component';
     MatMenu,
     MatMenuItem,
     MatDialogContent,
+    MatPaginatorModule,
   ],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  //paginator props
+  pageSize: number = 5;
+  pageIndex: number = 0;
+  pageSizeOptions: number[] = [5];
+
   viewMode: boolean = true;
   editMode: boolean = false;
   currDate: string = new Date().toLocaleDateString();
   readonly dialog = inject(MatDialog);
 
+  completeCheckBox: String = 'Complete';
+
   testService = inject(TestGoal);
+
+  get paginatedGoals() {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.testService.goals.slice(startIndex, endIndex);
+  }
+
+  // Handle page changes
+  onPageChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+  }
+
+  // Get total number of goals
+  get totalGoals() {
+    return this.testService.goals.length;
+  }
 
   addGoal() {
     this.dialog.open(AddGoalComponent);
@@ -68,6 +95,13 @@ export class Dashboard {
     this.viewMode = false;
     console.log(this.editMode);
   }
+
+  toggleCompletion(goal: Goal) {
+    goal.completed = !goal.completed;
+    this.completeCheckBox = goal.completed ? 'Completed' : 'Complete';
+    this.testService.goals.sort((a, b) => Number(a.completed) - Number(b.completed));
+  }
+
   public lineChartData: ChartConfiguration<'line'>['data'] = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
